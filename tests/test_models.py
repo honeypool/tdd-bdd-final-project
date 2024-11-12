14b7a22feab47bc636716cba31942275f5872af7
chpoklv@edx <chpoklv@gmail.com>
2024-11-12 15:43:14 -0500

    row 106 covered with test

--- tests/test_models.py
+++ tests/test_models.py
@@ -27,7 +27,7 @@ import os
 import logging
 import unittest
 from decimal import Decimal
-from service.models import Product, Category, db
+from service.models import Product, Category, db, DataValidationError
 from service import app
 from tests.factories import ProductFactory
 
@@ -132,6 +132,10 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(len(products), 1)
         self.assertEqual(products[0].id, original_id)
         self.assertEqual(products[0].description, "testing")
+        product.id = None
+        with self.assertRaises(DataValidationError):  # Replace ValueError with the actual exception raised if different
+            product.update()
+
     def test_delete_a_product(self):
         """It should Delete a Product"""
         product = ProductFactory()
